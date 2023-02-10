e9c9d0a963490a395d56f9b71fc1b3e89699b818
elad aviv <elad.aviv@workiz.com>
2023-02-10 15:07:40 +0200

    db config and validator for requests

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(
+        process.env.MONGODB_URI,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+        }
+      );
+      console.log('MongoDB Connected');
+    } catch (err) {
+      console.error(err.message);
+      // exit process with failure
+      process.exit(1);
+    }
+  };
+
+  module.exports = connectDB;
