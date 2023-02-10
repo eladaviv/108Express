86ad42374359db0ad9017e203bd2c3de0cc86967
Barak Eitan <barak.ha.meleh@gmail.com>
2023-02-10 15:49:04 +0200

    implement the checkout  functionality

---
+++ client/src/core/Checkout.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import {
+  getProducts,
+  createOrder,
+} from './apiCore';
+import { emptyCart } from './cartHelpers';
+import Card from './Card';
+import { isAuthenticated } from '../auth';
+import { Link } from 'react-router-dom';
+// import DropIn from 'braintree-web-drop-in-react';
+
+const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  const [data, setData] = useState({
+    loading: false,
+    success: false,
+    clientToken: null,
+    error: '',
+    instance: {},
+    address: '',
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    setData({ clientToken: token});
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, []);
+
+  const handleAddress = (event) => {
+    setData({ ...data, address: event.target.value });
+  };
+
+  const getTotal = () => {
+    return products.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+
+  const showCheckout = () => {
+    return isAuthenticated() ? (
+      <div>{showDropIn()}</div>
+    ) : (
+      <Link to='/signin'>
+        <Button variant='contained' color='primary'>
+          Sign in to checkout
+        </Button>
+      </Link>
+    );
+  };
+
+  let deliveryAddress = data.address;
+
+  const buy = () => {
+    setData({ loading: true });
+    const createOrderData = {
+      products: products,
+      transaction_id: "123456",
+      amount: getTotal(products),
+      address: deliveryAddress,
+    };
+
+    createOrder(userId, token, createOrderData)
+      .then((response) => {
+        emptyCart(() => {
+          setRun(!run); // run useEffect in parent Cart
+          console.log('payment success and empty cart');
+          setData({
+            loading: false,
+            success: true,
+          });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setData({ loading: false });
+      });
+
+  };
+
+  const showDropIn = () => (
+    <div onBlur={() => setData({ ...data, error: '' })}>
+      {data.clientToken !== null && products.length > 0 ? (
+        <div>
+          <div className='gorm-group mb-3'>
+            <label className='text-muted'>Delivery address:</label>
+            <textarea
+              onChange={handleAddress}
+              className='form-control'
+              value={data.address}
+              placeholder='Type your delivery address here...'
+            />
+          </div>
+
+          {/* <DropIn
+            options={{
+              authorization: data.clientToken,
+              paypal: {
+                flow: 'vault',
+              },
+            }}
+            onInstance={(instance) => (data.instance = instance)}
+          /> */}
+          <button onClick={buy} className='btn btn-success btn-block'>
+            Pay
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+
+  const showError = (error) => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = (success) => (
+    <div
+      className='alert alert-info'
+      style={{ display: success ? '' : 'none' }}
+    >
+      Thanks! Your payment was successful!
+    </div>
+  );
+
+  const showLoading = (loading) =>
+    loading && <h2 className='text-danger'>Loading...</h2>;
+
+  return (
+    <div>
+      <h2>Total: ${getTotal()}</h2>
+      {showLoading(data.loading)}
+      {showSuccess(data.success)}
+      {showError(data.error)}
+      {showCheckout()}
+    </div>
+  );
+};
+
+export default Checkout;
